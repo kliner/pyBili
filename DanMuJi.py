97f527fa60c753a09a920bcfce1315bd8ff8fe69
kliner <kliner@live.cn>
2017-03-13 23:02:51 +0800

    add MacOS system notification

--- DanMuJi.py
+++ DanMuJi.py
@@ -8,12 +8,15 @@ import time
 import json
 import threading
 import sys
+import subprocess
 
 DEBUG = 0
 
 class DanmakuHandler(bili.DanmakuHandler):
-    def __init__(self, startGiftResponse = False, showTime = True):
+    def __init__(self, startGiftResponse = False, showTime = False, showNotification = False):
         self.cnt = 9
+        self.notificationTimers = {}
+        self.showNotification = showNotification
         self.date_format = '%H:%M:%S'
         self.gifts = []
         self.LOCK = threading.Lock()
@@ -36,10 +39,13 @@ class DanmakuHandler(bili.DanmakuHandler):
             tm = time.strftime(self.date_format, danmaku.time)
             if 'info' in raw:
                 info = raw['info']
+                user, content = info[2][1].encode('utf-8'), info[1].encode('utf-8')
                 if self.showTime:
-                    print '[%s] \033[91m%s\033[0m : \033[94m%s\033[0m' % (tm, info[2][1].encode('utf-8'), info[1].encode('utf-8'))
+                    print '[%s] \033[91m%s\033[0m : \033[94m%s\033[0m' % (tm, user, content)
                 else:
-                    print '\033[91m%s\033[0m : \033[94m%s\033[0m' % (info[2][1].encode('utf-8'), info[1].encode('utf-8'))
+                    print '\033[91m%s\033[0m : \033[94m%s\033[0m' % (user, content)
+                if self.showNotification:
+                    self.showMacNotification(user, content)
             elif raw['cmd'] == 'SEND_GIFT':
                 data = raw['data']
                 uname, num, giftName = data['uname'].encode('utf-8'), data['num'], data['giftName'].encode('utf-8')
@@ -74,16 +80,33 @@ class DanmakuHandler(bili.DanmakuHandler):
             bili_sender.init()
             if DEBUG: print 'giftResponseThreadStarted!'
 
+    def showMacNotification(self, title, content):
+
+        cmd = "terminal-notifier -title '%s' -message '%s' -group '%s' > /dev/null" % (title, content, title)
+        # cmd = 'osascript -e \'display notification "%s" with title "%s"\'' % (content, title)
+        subprocess.Popen(cmd, shell=True)
+        t = threading.Timer(10, self.closeMacNotification, (title, ))
+        t.start()
+        if title in self.notificationTimers: self.notificationTimers[title].cancel()
+        self.notificationTimers[title] = t
+
+    def closeMacNotification(self, group):
+        cmd = "terminal-notifier -remove '%s' > /dev/null" % group
+        subprocess.Popen(cmd, shell=True)
+
+
+
 if __name__ == '__main__':
     argv = sys.argv
     roomid = 90012
+    startGiftResponse, showTime, showNotification = False, False, False
     if len(argv) == 2:
         roomid = int(argv[1])
     if len(argv) == 3:
         roomid = int(argv[1])
-        showTime = int(argv[2])
+        startGiftResponse, showTime, showNotification = int(argv[2][0]), int(argv[2][1]), int(argv[2][2]) 
 
-    py = bili.BiliHelper(roomid, DanmakuHandler(startGiftResponse = True, showTime = showTime))
+    py = bili.BiliHelper(roomid, DanmakuHandler(startGiftResponse = startGiftResponse, showTime = showTime, showNotification = showNotification))
     while 1:
         cmd = raw_input()
         bili_sender.sendDanmaku(roomid, cmd)
