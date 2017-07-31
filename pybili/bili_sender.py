6c680cfb5de75896df6ad7b0af5b277a357097d7
kliner <kliner@live.cn>
2017-07-31 20:58:09 +0300

    refactor get/set

--- pybili/bili_sender.py
+++ pybili/bili_sender.py
@@ -10,8 +10,6 @@ import logging.handlers
 
 SEND_URL = 'http://live.bilibili.com/msg/send'
 TV_URL = 'http://api.live.bilibili.com/SmallTV/join'
-QUERY_NEED_YOU = 'http://api.live.bilibili.com/activity/v1/NeedYou/getLiveInfo'
-NEED_YOU_URL = 'http://api.live.bilibili.com/activity/v1/NeedYou/getLiveAward'
 QUERY_SUMMER_URL = 'http://api.live.bilibili.com/activity/v1/SummerBattle/check'
 SUMMER_URL = 'http://api.live.bilibili.com/activity/v1/SummerBattle/join'
 
@@ -34,10 +32,27 @@ class Sender(object):
         if not logger.handlers:
             self._initLogger(logger)
         
-        
         self.cookies = cookies
         self.lightenIds = set()
         self.raffleIds = set()
+
+    def _get(self, url, params):
+        r = requests.get(url, params=params, cookies=self.cookies)
+        result = r.content
+        raw = json.loads(result)
+        if raw['code'] != 0: 
+            self.logger.debug(raw)
+            self.logger.warn(raw['msg'])
+        else: return raw
+
+    def _post(self, url, params):
+        r = requests.post(url, data=params, cookies=self.cookies)
+        result = r.content
+        raw = json.loads(result)
+        if raw['code'] != 0: 
+            self.logger.debug(raw)
+            self.logger.warn(raw['msg'])
+        else: return raw
 
     def sendDanmaku(self, roomid, content, color='white'):
         try:
@@ -54,77 +69,33 @@ class Sender(object):
                 "rnd":int(time.time()),
                 "roomid":roomid
                 }
-            r = requests.post(SEND_URL, data=params, cookies=self.cookies)
-            result = r.content
-            raw = json.loads(result)
-            if raw['code'] != 0: self.logger.warn(raw['msg'])
-            return True
+            return self._post(SEND_URL, params)
         except:
             return False
 
-    
     def joinSmallTV(self, roomid, tv_id):
         params = {
                 'roomid':roomid,
                 'id':tv_id,
                 '_':int(time.time() * 100)
                 }
-        r = requests.get(TV_URL, params=params, cookies=self.cookies)
-        result = r.content
-        raw = json.loads(result)
-        if raw['code'] != 0: self.logger.warn(raw['msg'])
-
-    def _joinNeedYou(self, roomid, lightenId):
-        params = {
-                'roomid':roomid,
-                'lightenId':lightenId
-                }
-        r = requests.post(NEED_YOU_URL, data=params, cookies=self.cookies)
-        result = r.content
-        raw = json.loads(result)
-        #if raw['code'] != 0: print raw['msg']
-        #print raw['msg']
-
-    def joinNeedYou(self, roomid):
-        params = {
-                'roomid':roomid
-                }
-        r = requests.get(QUERY_NEED_YOU, params=params, cookies=self.cookies)
-        result = r.content
-        raw = json.loads(result)
-        if raw['code'] == 0 and 'lightenId' in result:
-            for d in raw['data']:
-                lightenId = d['lightenId']
-                if lightenId not in self.lightenIds:
-                    self._joinNeedYou(roomid, lightenId)
-                    self.lightenIds.add(lightenId)
-
-        #if raw['code'] != 0: print raw['msg']
-        #print self.lightenIds
+        self._get(TV_URL, params)
 
     def _joinSummer(self, roomid, raffleId):
         params = {
                 'roomid':roomid,
                 'raffleId':raffleId
                 }
-        r = requests.post(SUMMER_URL, data=params, cookies=self.cookies)
-        result = r.content
-        raw = json.loads(result)
-        if raw['code'] != 0: self.logger.warn(raw['msg'])
-        self.logger.debug(raw['msg'])
+        r = self._post(SUMMER_URL, params)
+        if r: self.logger.debug('join summer: %s' % r['msg'])
 
     def joinSummer(self, roomid, giftId):
         params = {
                 'roomid':roomid
                 }
-        self.logger.debug(params)
-        r = requests.get(QUERY_SUMMER_URL, params=params, cookies=self.cookies)
-        result = r.content
-        raw = json.loads(result)
-        self.logger.debug(raw)
-        #{"code":0,"msg":"success","message":"success","data":[{"raffleId":"13878","type":"waterBattle","from":"bchdy","time":59,"status":1}]}
-        if raw['code'] == 0:
-            for d in raw['data']:
+        r = self._get(QUERY_SUMMER_URL, params)
+        if r:
+            for d in r['data']:
                 raffleId = d['raffleId']
                 if raffleId not in self.raffleIds:
                     self._joinSummer(roomid, raffleId)
@@ -138,16 +109,11 @@ class Sender(object):
                 'roomid':roomid,
                 'raffleId':raffleId
                 }
-        r = requests.get(url, params=params, cookies=self.cookies)
-        result = r.content
-        raw = json.loads(result)
-        if raw['code'] == 0:
-            if raw['data']['gift_id'] == 76:
-                self.logger.info('get!')
-            elif raw['data']['gift_id'] == -1:
-                self.logger.info('empty!')
-            else:
-                self.logger.warn(raw['data'])
+        r = self._get(url, params)
+        if r:
+            if r['data']['gift_id'] == 76: self.logger.info('get!')
+            elif r['data']['gift_id'] == -1: self.logger.info('empty!')
+            else: self.logger.warn(raw['data'])
 
 def main():
     import bili_config
