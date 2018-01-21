13389c32ccc71e84b54ece6b288823d0278f8639
kliner <kliner@live.cn>
2018-01-21 20:52:28 +0300

    fix raffle. v0.3.3

--- pybili/__init__.py
+++ pybili/__init__.py
@@ -2,11 +2,11 @@ import os
 import logging
 
 __author__ = 'kliner'
-__version__ = '0.3.3.a0'
+__version__ = '0.3.3'
 
 # init config & temp dir
 home = os.path.expanduser("~")
 __workdir__ = os.path.join(home, '.pybili')
 if not os.path.exists(__workdir__):
     os.makedirs(__workdir__)
-__loglevel__ = logging.DEBUG
+__loglevel__ = logging.ERROR
